1fb615d671d57536b7f820eaa98cbeb3318c4ae9
Patrick Thibaudeau <thibaud@yorku.ca>
2024-08-04 09:03:16 -0400

    Added comment to update plugin version in DB

--- version.php
+++ version.php
@@ -19,3 +19,5 @@ $plugin->requires = 2022112800; //Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
 $plugin->component = 'local_cria';
+
+//SELECT * FROM `mdl_config_plugins` WHERE plugin='local_cria' AND name='version';
